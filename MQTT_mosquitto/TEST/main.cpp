dd535febafcbe9ce780b7d680fffa29f53d51dd1
cyniu88 <cyniu88@wp.pl>
2019-01-13 21:11:05 +0100

    dodano BT  i travisa CI

--- MQTT_mosquitto/TEST/main.cpp
+++ MQTT_mosquitto/TEST/main.cpp
@@ -1,31 +1,43 @@
 ﻿#include <stdio.h>
-#include <mosquitto.h>
 #include <string>
 #include <thread>
 #include <iostream>
 #include "../mqtt.h"
+#include <gtest/gtest.h>
 
-int main(int argc, char *argv[])
+class MQTT_BT : public ::testing::Test
+{
+public:
+    std::string topicSubscribe = "iDom/#";
+    std::string topicPublish = "iDom/bt";
+    std::string host = "localhost";
+};
+
+TEST_F(MQTT_BT, wrong_host)
+{
+    EXPECT_ANY_THROW(MQTT_mosquitto("test_iDomServer","fake host",1883,60,false));
+}
+
+TEST_F(MQTT_BT, flow)
 {
     int ilosc = 30;
     /// iot.eclipse.org
-    MQTT_mosquitto mq("test_iDomServer","192.168.4.1",1883,60,false);
-    mq.turnOffDebugeMode();
+    MQTT_mosquitto mq("test_iDomServer",host,1883,60,false);
+    mq.turnOnDebugeMode();
 
-    puts("odlapalmy subscribera MQTT w nowym watku");
-    auto th1 = std::thread(MQTT_mosquitto::subscribeHandlerRunInThread,&mq, "iDom/#" , 2);
+    puts("odpalmy subscribera MQTT w nowym watku");
+    auto th1 = std::thread(MQTT_mosquitto::subscribeHandlerRunInThread,&mq, topicSubscribe.c_str() , 2);
     th1.detach();
 
     for(int i = 1; i < ilosc+1 ; ++ i)
     {
-        mq.publish("iDom/pies", "test message: " + std::to_string(i),0);
+        mq.publish(topicPublish, "test message: " + std::to_string(i),0);
         std::cout << "wysłano wiadomosc numer: " << i << std::endl;
     }
     std::this_thread::sleep_for(std::chrono::seconds(ilosc/3));
     int ssize = MQTT_mosquitto::getReceiveQueueSize();
     if( ssize != ilosc){
-        std::cout <<"error  nie odebralem wszystkich wiadomosci " << ssize << std::endl;
-        return 1;
+        FAIL() <<"error  nie odebralem wszystkich wiadomosci " << ssize << std::endl;
     }
     else {
         std::cout << "gra ilosc wiadomosci odebranych " << ssize << std::endl;
@@ -38,11 +50,22 @@ int main(int argc, char *argv[])
             std::cout << "jest ok porownanie: " << i << std::endl;
         }
         else {
-            std::cout << "a to nie jest ok porownanie: " << i << std::endl;
-            return 1;
+            FAIL() << "a to nie jest ok porownanie: " << i << std::endl;
         }
     }
     mq.disconnect();
 
-    return 0;
+}
+int main(int argc, char *argv[])
+{
+
+    ::testing::InitGoogleTest( &argc, argv );
+    //::testing::GTEST_FLAG(filter) = "functions_fixture.sleepThread";
+    //::testing::GTEST_FLAG(filter) = "event_counter_fixture.getLast1minNumberEvent";
+    //::testing::GTEST_FLAG(filter) = "command433MHz_Class_fixture.*)";
+    auto ret = RUN_ALL_TESTS();
+
+    const char * PROG_INFO = " Basic TEST iDomServer: " __DATE__ ", " __TIME__;
+    std::cout << "wersja " <<PROG_INFO <<" "<< GIT_BRANCH <<" " << GIT_COMMIT_HASH << std::endl;
+    return ret;
 }
